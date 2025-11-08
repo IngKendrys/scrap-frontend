b141a590f15591b018f525db477ac214fa104842
IngKendrys <kendrys333@gmail.com>
2025-11-08 00:09:15 -0500

    cambios

--- src/app/home/page.js
+++ src/app/home/page.js
@@ -5,7 +5,7 @@ export default function HomePage() {
     const {user} = useSelector((state) => state.auth);
     return (
         <div>
-            <h1>Welcome to the Home Page {user?.nombre_negocio}</h1>
+            <h1>Cargando {user?.nombre_negocio}...</h1>
         </div>
     );
 }
